3b71017e5c6c0f7ba9e51895a5f8387148748d27
Jonathon Lin <jonathonlin93@yahoo.com>
2017-03-08 23:26:02 -0800

    fix bug in makeChange

--- app/coinCounter.js
+++ app/coinCounter.js
@@ -1,39 +1,52 @@
-function coinCounter(total, coins) {
-  if (total <= 0) {
+function makeChange(target, coins) {
+  if (target === 0) {
     return [];
   }
 
-  if (coins.every(el => el > total)) {
+  if (coins.every(el => el > target)) {
     return null;
   }
 
   let bestChange = null;
 
-  coins.sort((a,b) => {return b-a;});
-  coins.forEach((coin, index) => {
-    if (coin > total) {
+  function reverseSorter(a, b) {
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+
+  coins.sort(reverseSorter).forEach((coin, index) => {
+    if (coin > target) {
       return;
     }
-    let remainder = total - coin;
-    let restChange = coinCounter(remainder, coins.slice(index));
+
+    let remainder = target - coin;
+
+    let restChange = makeChange(remainder, coins.slice(index));
+
     if (!restChange) {
       return;
     }
+
     let change = [coin].concat(restChange);
-    if (!bestChange || (change.length <= bestChange.length)) {
+    if (!bestChange || (change.length < bestChange.length)) {
       bestChange = change;
     }
   });
+
   return bestChange;
 }
 
 
-
 document.getElementById('make-change').addEventListener("click", function(){
-  let total = document.getElementById('total').value;
-  let one = document.getElementById('coin-one').value;
-  let two = document.getElementById('coin-two').value;
-  let three = document.getElementById('coin-three').value;
-  let four = document.getElementById('coin-four').value;
-  console.log(coinCounter(total, [one,two,three,four]))
+  let target = parseInt(document.getElementById('total').value);
+  let one = parseInt(document.getElementById('coin-one').value);
+  let two = parseInt(document.getElementById('coin-two').value);
+  let three = parseInt(document.getElementById('coin-three').value);
+  let four = parseInt(document.getElementById('coin-four').value);
+  console.log(makeChange(target, [one,two,three,four]))
 });
